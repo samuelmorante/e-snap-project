5572aa70911170350f923c40645f8ccc0360f19f
samuelmorante <samuelmorantep@gmail.com>
2026-01-26 11:16:12 +0100

    Agregando carpeta del backend con el initialzr de Spring Boot

---
+++ backend/src/main/java/com/esnap/backend/BackendEsnapApplication.java
@@ -0,0 +1,13 @@
+package com.esnap.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEsnapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEsnapApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/esnap/backend/BackendEsnapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esnap.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEsnapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
